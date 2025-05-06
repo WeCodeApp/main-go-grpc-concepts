2c9e449a0cad98a4fd4250f6c842c5715c26760f
shairacruz <shairayvonnecruz028@gmail.com>
2025-05-06 10:10:03 +0800

    Add server side and client side streaming

--- grpc_streaming/client/main.go
+++ grpc_streaming/client/main.go
@@ -1,1 +1,66 @@
-package client
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	mainpb "grpc_stream_client/proto/gen"
+	"io"
+	"log"
+	"time"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer conn.Close()
+
+	client := mainpb.NewCalculateClient(conn)
+
+	ctx := context.Background()
+	// -------- SERVER SIDE STREAMING STARTS
+	req := &mainpb.FibonacciRequest{
+		N: 10,
+	}
+	stream, err := client.GenerateFibonacci(ctx, req)
+	if err != nil {
+		log.Fatalln("Error calling GenerateFibonacci func:", err)
+	}
+
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			log.Println("End of stream")
+			break
+		}
+		if err != nil {
+			log.Fatalln("Error receiving data from GenerateFibonacci func:", err)
+		}
+		log.Println("Fibonacci number: ", resp.GetNumber())
+	}
+
+	// CLIENT SIDE STREAMING
+
+	stream1, err := client.SendNumbers(ctx)
+	if err != nil {
+		log.Fatalln("Error creating stream:", err)
+	}
+
+	for num := range 9 {
+		log.Println("Sending number: ", num)
+		err := stream1.Send(&mainpb.NumberRequest{Number: int32(num)})
+		if err != nil {
+			log.Fatalln("Error sending data:", err)
+		}
+		time.Sleep(time.Second)
+	}
+
+	res, err := stream1.CloseAndRecv()
+	if err != nil {
+		log.Fatalln("Error receiving response:", err)
+	}
+
+	log.Println("Sum: ", res.GetSum())
+}
--- grpc_streaming/server/main.go
+++ grpc_streaming/server/main.go
@@ -1,1 +1,64 @@
-package server
+package main
+
+import (
+	"google.golang.org/grpc"
+	mainpb "grpc_stream_server/proto/gen"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+type server struct {
+	mainpb.UnimplementedCalculateServer
+}
+
+func (s *server) GenerateFibonacci(req *mainpb.FibonacciRequest, stream mainpb.Calculate_GenerateFibonacciServer) error {
+	n := req.N
+	a, b := 0, 1
+
+	for i := 0; i < int(n); i++ {
+		err := stream.Send(&mainpb.FibonacciResponse{
+			Number: int32(a),
+		})
+		if err != nil {
+			return err
+		}
+		log.Println("Sent number:", a)
+		a, b = b, a+b
+		time.Sleep(time.Second)
+	}
+	return nil
+}
+
+func (s *server) SendNumbers(stream mainpb.Calculate_SendNumbersServer) error {
+	var sum int32
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&mainpb.NumberResponse{Sum: sum})
+		}
+		if err != nil {
+			return err
+		}
+		log.Println(req.GetNumber())
+		sum += req.GetNumber()
+	}
+}
+
+func main() {
+	port := ":50051"
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	mainpb.RegisterCalculateServer(grpcServer, &server{})
+
+	log.Println("Server started on port: ", port)
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
