fdaf03abb6e3ef19d71d6a6a259cdc2a3972ed65
shairacruz <shairayvonnecruz028@gmail.com>
2025-05-06 09:27:34 +0800

    Add services to server

--- grpc_client/main.go
+++ grpc_client/main.go
@@ -6,6 +6,7 @@ import (
 	"google.golang.org/grpc/credentials"
 	"log"
 	mainpb "simplegrpcclient/proto/gen"
+	farewellpb "simplegrpcclient/proto/gen/farewell"
 	"time"
 )
 
@@ -29,6 +30,31 @@ func main() {
 	client := mainpb.NewCalculateClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
+
+	client2 := mainpb.NewGreeterClient(conn)
+
+	reqGreet := mainpb.HelloRequest{
+		Name: "John",
+	}
+	res1, err := client2.Greet(ctx, &reqGreet)
+	if err != nil {
+		log.Fatal("Failed to greet", err)
+	}
+
+	log.Println("Greeting: ", res1.Message)
+
+	fwCLient := farewellpb.NewFarewellClient(conn)
+
+	reqFarewell := farewellpb.GoodByeRequest{
+		Name: "Pedro",
+	}
+
+	resFw, err := fwCLient.BidGoodbye(ctx, &reqFarewell)
+	if err != nil {
+		log.Fatal("Failed to bid good bye", err)
+	}
+
+	log.Println("BidGoodBye: ", resFw.Message)
 
 	req := mainpb.AddRequest{
 		A: 10,
--- grpc_server/main.go
+++ grpc_server/main.go
@@ -3,27 +3,51 @@ package main
 import (
 	"context"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 
 	pb "simplegrpcserver/proto/gen"
+	"simplegrpcserver/proto/gen/farewell"
 )
 
 type server struct {
 	pb.UnimplementedCalculateServer
+	pb.UnimplementedGreeterServer
+	farewellpb.UnimplementedFarewellServer
 }
 
 func (s *server) Add(ctx context.Context, req *pb.AddRequest) (*pb.AddResponse, error) {
 	return &pb.AddResponse{Sum: req.A + req.B}, nil
 }
 
+func (s *server) Greet(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	return &pb.HelloResponse{Message: "Hello " + req.Name}, nil
+}
+
+func (s *server) BidGoodbye(ctx context.Context, req *farewellpb.GoodByeRequest) (*farewellpb.GoodByeResponse, error) {
+	return &farewellpb.GoodByeResponse{Message: "Goodbye " + req.Name}, nil
+}
+
 func main() {
+	cert := "cert.pem"
+	key := "key.pem"
+
 	port := ":50051"
 	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
-	grpcServer := grpc.NewServer()
+	creds, err := credentials.NewServerTLSFromFile(cert, key)
+	if err != nil {
+		log.Fatalf("failed to create creds: %v", err)
+	}
+	grpcServer := grpc.NewServer(grpc.Creds(creds))
 
 	pb.RegisterCalculateServer(grpcServer, &server{})
+	pb.RegisterGreeterServer(grpcServer, &server{})
+	farewellpb.RegisterFarewellServer(grpcServer, &server{})
 
 	log.Println("Server started on port: ", port)
 	err = grpcServer.Serve(lis)
