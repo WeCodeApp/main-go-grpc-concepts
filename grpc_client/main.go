30bada1e6bfcf5b76e15cefbbb34910a30c74ed1
shairacruz <shairayvonnecruz028@gmail.com>
2025-05-06 09:01:54 +0800

    Add new conf for generating self signed cert

--- grpc_client/main.go
+++ grpc_client/main.go
@@ -3,14 +3,24 @@ package main
 import (
 	"context"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 	"log"
 	mainpb "simplegrpcclient/proto/gen"
 	"time"
 )
 
 func main() {
-	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	cert := "cert.pem"
+	creds, err := credentials.NewClientTLSFromFile(cert, "")
+	if err != nil {
+		log.Fatalf("failed to create creds: %v", err)
+	}
+
+	conn, err := grpc.NewClient(
+		"localhost:50051",
+		grpc.WithTransportCredentials(creds),
+	)
 	if err != nil {
 		log.Fatal("Failed to connect", err)
 	}
